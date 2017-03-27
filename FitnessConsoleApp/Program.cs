27f47343a62ac8111aa010479621d8f08b0b8fb7
MoLeighJackson <moleighjackson@gmail.com>
2017-03-27 19:06:24 -0400

    added files
    from project created in Visual Studio

---
+++ FitnessConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int totalMinsExercised = 0;
+            bool continueTracking = true;
+
+            while (continueTracking)
+            {
+                // 1. Prompt user for minutes exercised
+
+                Console.Write("How long have you been exercising? (in minutes) or enter \'QUIT\' to exit program: ");
+
+                // 2. Read user input - create variable for user input, initialize variable
+
+                string userTimeInput = Console.ReadLine();
+
+                // 3. Parse the integer entered by user - display mins exercised to the screen
+
+                int minutes = int.Parse(userTimeInput);
+                totalMinsExercised = totalMinsExercised + minutes;
+
+                Console.WriteLine("You entered " + userTimeInput + " mins");
+                Console.WriteLine("You have been exercising for " + totalMinsExercised + " mins total");
+
+            }
+        }
+    }
+}
